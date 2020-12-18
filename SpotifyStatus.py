e96544e8259f94dcf3faa4221d5ac6507e739645
Evan Shrestha <evanshrestha@gmail.com>
2020-12-18 03:31:15 +0000

    Initial commit

--- SpotifyStatus.py
+++ SpotifyStatus.py
@@ -10,15 +10,15 @@ def combine_list_str(str_list):
     return_str = ''
     for i in range(len(str_list) - 1):
         if len(str_list) - i == 2:
-            return_str += str_list[i] + ' and ' + str_list[i + 1]
+            return_str += str_list[i] + ', and ' + str_list[i + 1]
         else:
             return_str += str_list[i] + ', '
     return return_str
 
-sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id="01f1bdc7c6f4481c98c980889567dc17",
-                                               client_secret="58ae618c179947e6962e0fa8c54d07dd",
-                                               redirect_uri="http://evanshrestha.com",
-                                               scope="user-read-currently-playing"))
+
+sp = spotipy.Spotify(auth_manager=SpotifyOAuth(redirect_uri="http://evanshrestha.com",
+                                               scope="user-read-currently-playing",
+                                               open_browser=False))
 
 results = sp.current_user_playing_track()
 
