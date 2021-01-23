57ea5011db00147f320e373d91c3d076408e768c
Evan Shrestha <evanshrestha@gmail.com>
2021-01-23 22:38:09 +0000

    Add updates to server script

---
+++ server.py
@@ -0,0 +1,113 @@
+from flask import Flask, render_template, jsonify
+import time
+import threading
+from spotify_status import SpotifyStatus
+app = Flask(__name__)
+
+current_playing = None
+
+def get_current_song_dict():
+    global current_playing
+    if isinstance(current_playing, str):
+        current_playing = None
+
+    curr_song = None
+    if current_playing is not None:
+
+        curr_artists = []
+
+        for a in current_playing['artists']:
+            curr_artists.append({
+                'name': a['name'],
+                'link': a['external_urls']['spotify']
+            })
+
+        curr_song = {
+            'name': current_playing['name'],
+            'artists': curr_artists,
+            'link': current_playing['external_urls']['spotify'],
+            'image': get_current_playing_image()
+        }
+    return curr_song
+
+@app.route('/')
+def index():
+    curr_song = get_current_song_dict()
+    return render_template('index.html', curr_song = curr_song)
+
+@app.route('/api/<request>/')
+def api(request):
+    if request == 'current':
+        return jsonify(get_current_song_dict())
+    else:
+        return 'Unknown request'
+
+def combine_list_str(str_list) -> str:
+    """
+    Combine strings with commas and/or "and"s appropriately.
+    
+    Args:
+        str_list: A list containing the strings to be combined.
+    Returns:
+        The final output string.
+    """
+    if len(str_list) == 1:
+        return str_list[0]
+    if len(str_list) == 2:
+        return str_list[0] + ' and ' + str_list[1]
+
+    return_str = ''
+    for i in range(len(str_list) - 1):
+        if len(str_list) - i == 2:
+            return_str += str_list[i] + ', and ' + str_list[i + 1]
+        else:
+            return_str += str_list[i] + ', '
+    return return_str
+
+def get_current_playing_image() -> str:
+    """
+    Get the currently playing image link.
+
+    Returns:
+        The currently playing track's image reference.
+    """
+    global current_playing
+    if current_playing is None:
+        return None
+    else:
+        try:
+            return current_playing['album']['images'][0]['url']
+        except Exception as e:
+            print(e)
+            return None
+
+def get_current_playing_string() -> str:
+    """
+    Get the currently playing track.
+    
+    Returns:
+        The currently playing track in string format.
+    """
+    global current_playing
+    if current_playing is None:
+        return "Evan's not listening to anything right now. Maybe later!"
+    else:
+        song = current_playing['name']
+        song_link = current_playing['external_urls']['spotify']
+        artists = combine_list_str(['<a href="{}"'.format(a['external_urls']['spotify']) + '>' + a['name'] + '</a>' for a in current_playing['album']['artists']])
+
+        return """
+        Evan's listening to <a href="{song_link}">{song}</a> by {artists}.
+        """.format(song=song, song_link=song_link, artists=artists)
+
+def update_current_playing():
+    global current_playing
+    while True:
+        current_playing = ss.get_current_playing()
+        time.sleep(5)
+
+if __name__ == '__main__':
+    ss = SpotifyStatus()
+    update_thread = threading.Thread(target=update_current_playing)
+    update_thread.start()
+    app.run(host='0.0.0.0', port=5002)
--- spotify_status.py
+++ spotify_status.py
@@ -1,6 +1,6 @@
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
-
+import time
 
 def combine_list_str(str_list):
     if len(str_list) == 1:
@@ -16,24 +16,41 @@ def combine_list_str(str_list):
     return return_str
 
 class SpotifyStatus:
+
     def __init__(self):
-        self.sp = spotipy.Spotify(auth_manager=SpotifyOAuth(redirect_uri="http://evanshrestha.com",
-                                               scope="user-read-currently-playing",
+        self.sp = spotipy.Spotify(auth_manager=SpotifyOAuth(
+                                               scope="user-read-currently-playing,user-read-recently-played,user-read-playback-state",
                                                open_browser=False))
     def get_current_playing(self):
-        results = self.sp.current_user_playing_track()
+        results = self.sp.currently_playing()
 
         if results is None:
             return 'Not currently playing a track'
         else:
             song = results['item']
-            title = song['name']
-            artists = [a['name'] for a in song['artists']]
-            return f'Now playing {title} by {combine_list_str(artists)}'
+            return song
+            # title = song['name']
+            # artists = [a['name'] for a in song['artists']]
+            # return f'Now playing {title} by {combine_list_str(artists)}'
+    
+    def get_recently_played(self):
+        results = self.sp.current_user_recently_played()
+        song_list = []
+        #return_str = ''
+        if results is None:
+            return_str = 'No recent tracks'
+        else:
+            for item in results['items']:
+                song_list.append(item)
+                #return_str += (item['track']['name'] + ' by ' + combine_list_str([a['name'] for a in item['track']['artists']])) + '\n'
+        #return return_str
+        return song_list
 
 def main():
     ss = SpotifyStatus()
-    print(ss.get_current_playing())
+    while True:
+        print(ss.get_current_playing())
+        time.sleep(60)
 
 if __name__ == '__main__':
     main()
